bc8e4de3dc8b68a06c65ae658817718847e10faa
Yasmin <yasminsdcastro@gmail.com>
2022-11-01 19:48:31 -0300

    feat: aula 12

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css'],
+})
+export class ListRenderComponent implements OnInit {
+  animals = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
